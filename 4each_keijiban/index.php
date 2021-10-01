940c68741759ae71c5f5bfb63aa22cd63ad4f182
OKUDAAKIRA <09daakira@gmail.com>
2021-10-01 14:45:57 +0900

    コミット

---
+++ 4each_keijiban/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>4eachblog 掲示板</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body>
+        
+        <?php
+        mb_internal_encoding("utf8");
+        $pdo = new PDO("mysql:dbname=lesson00;host=localhost;","root","");
+        $stmt = $pdo->query("select * from 4each_keijiban");
+        
+        
+        
+        ?>
+        
+        
+  <div class="logo"> <img src="4eachblog_logo.jpg"></div>
+
+    <header>
+        
+        <ul>
+            <li>トップ</li>
+            <li>プロフィール</li>
+            <li>4eachについて</li>
+            <li>登録フォーム</li>
+            <li>問い合わせ</li>
+            <li>その他</li>
+        </ul>
+        
+    
+        
+    </header>        
+    
+    <main>
+        <div class="main-container">
+            
+            <div class="left">
+            
+            <h2>プログラミングに役立つ掲示板</h2>
+            
+                    
+        <form method="post" action="insert.php">
+            
+            <h3>入力フォーム</h3>
+            <div>
+                <label>ハンドルネーム</label>
+                <br>
+                <input type="text" class="text" size="35" name="handlename">
+            </div>
+            
+            <div>
+                <label>タイトル</label>
+                <br>
+                <input type="text" class="text" size="35" name="title">
+            </div>
+            
+            
+            <div>
+                <label>コメント</label>
+            <br>
+            <textarea cols="60" rows="7" name="comments"></textarea>
+        
+            </div>
+            <div>
+                <input type="submit" class="submit" value="投稿する">
+                </div>
+        </form>
+            <?php
+                
+                while($row=$stmt->fetch()){
+                
+                echo"<div class='gray_box1'>";
+                    echo "<h3>".$row['title']."</h3>";
+                    echo "<div class='contents'>";
+                    echo $row['comments'];
+                    echo "<div class='handlename'>posted by".$row['handlename']."</div>";
+                echo "</div>";
+                echo"</div>";
+                }
+                
+                ?>
+                
+                <div class="gray_box2">
+                    <h3>タイトル</h3>
+                    <p>記事の中身</p>
+                    <div class='handlename'>posted by 通りすがり</div>
+                </div>
+           
+            </div>
+            
+            
+            <div class="right">
+                <h3>人気の記事</h3>
+                <ul>
+                    <li>PHPのオススメ本</li>
+                    <li>PHP MyAdminの使い方</li>
+                    <li>今人気のエディタ</li>
+                    <li>HTMLの基礎</li>
+                </ul>
+                
+                <h3>オススメリンク</h3>
+                <ul>
+                    <li>インターノウス株式会社</li>
+                    <li>XAMPPのダウンロード</li>
+                    <li>Eclipscのダウンロード</li>
+                    <li>Bracketsのダウンロード</li>
+                </ul>
+                
+                <h3>カテゴリ</h3>
+                <ul>
+                    <li>HTML</li>
+                    <li>PHP</li>
+                    <li>MySQL</li>
+                    <li>JavaScript</li>
+                </ul>
+
+            
+            </div>
+        </div>
+            
+    </main>
+    
+    <footer>
+        copyright © inthernous | 4each blog the which provides A to Z about programming.
+    </footer>
+      
+    </body>
+    
+</html>
